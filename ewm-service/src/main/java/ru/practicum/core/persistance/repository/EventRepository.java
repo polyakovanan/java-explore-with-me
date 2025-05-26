e44eb9c103f743a6b83dda7b7abb9f228d3e6d41
Anatolii Poliakov <polyakovan@terralink-global.com>
2025-05-26 23:58:23 +0500

    Попытка обойти хитрый баг хибернейта

--- ewm-service/src/main/java/ru/practicum/core/persistance/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/core/persistance/repository/EventRepository.java
@@ -16,9 +16,9 @@ import java.util.List;
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT e FROM events e " +
-            "WHERE (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
+            "WHERE (LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
             "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')) " +
-            "OR LOWER(e.title) LIKE LOWER(CONCAT('%', :text, '%'))) " +
+            "OR LOWER(e.title) LIKE LOWER(CONCAT('%', :text, '%')) OR :text IS NULL) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND ((CAST(:rangeStart as DATE) IS NULL AND CAST(:rangeEnd as DATE) IS NULL AND e.eventDate > :currentTime) " +
