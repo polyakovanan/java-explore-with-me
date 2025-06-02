48789d6342ebdf2f5e4f8e7ea8a02e008a66f5dc
Anatolii Poliakov <polyakovan@terralink-global.com>
2025-06-03 01:07:50 +0500

    Этап 3 дипломный проект Serializable для составного ключа

--- ewm-service/src/main/java/ru/practicum/core/persistance/model/User.java
+++ ewm-service/src/main/java/ru/practicum/core/persistance/model/User.java
@@ -3,13 +3,15 @@ package ru.practicum.core.persistance.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Entity(name = "users")
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
